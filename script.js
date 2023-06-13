b302836f066b1e64df157fbcbee3097da82c9e04
serbayserdar <serbayserdar8@gmail.com>
2023-06-13 13:39:54 +0700

    es6 practice 1st part

---
+++ script.js
@@ -0,0 +1,37 @@
+function greeting (name) {
+    return 'Hello ' + name;
+}
+
+console.log(greeting('Peter'));
+
+// const greeting2 = (name) => {
+//     return 'Hello ' + name;
+// }
+
+// console.log(greeting('Peter'));
+
+
+const greeting2 = (name) => 'hello ' + name;
+
+console.log(greeting2('peter'));
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+// const numbersDoubled = numbers.map(function (item) {
+// return item * 2;
+// })
+
+
+const numbersDoubled = numbers.map((item) => item * 2);
+
+console.log('numbersDoubled ' + numbersDoubled);
+
+
+
+
+const people = [
+    {name: 'John', age: 25},
+    {name: 'Mary', age: 30},
+    {name: 'Bob', age: 35}
+];
